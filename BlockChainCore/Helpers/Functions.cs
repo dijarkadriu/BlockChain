6ffd7366aa47ab160c20f06cee077c9b599c5795
Dijar Kadriu <ddijarr@gmail.com>
2020-03-10 14:58:40 +0100

    Added CopyFiles to the functions

--- BlockChainCore/Helpers/Functions.cs
+++ BlockChainCore/Helpers/Functions.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace BlockChainCore.Helpers
 {
     public static class Functions
     {
+        public static void CopyFiles(string name, string extension, string fullPath)
+        {
+            string date = DateTime.Now.ToString().Replace('-', ' ').Replace(':', ' ').Trim();
+            string path = @"C:\Users\ddija\Desktop\CopiedFiles\" + name + date + extension;
+
+            File.Copy(fullPath, path);
+        }
     }
 }
